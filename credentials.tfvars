cdac64858cb6027baf3fa540147c87fb1b332d40
Ab1ss <abys.power@gmail.com>
2023-11-30 00:00:29 +0200

    test

---
+++ credentials.tfvars
@@ -0,0 +1,3 @@
+proxmox_api_url = "https://195.160.220.57/api2/json"
+proxmox_api_token_id = "root@pam!terraform"
+proxmox_api_token_secret = "14382600-9354-4d36-8cca-fc912037faa7"
--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
-some code for test
+resource "proxmox_vm_qemu" "test_terra" {
+    name = "test-terra-1"
+    desc = "Ubuntu server"
+    vmid = "1002"
+    target_node = "pve"
+
+    agent = 1
+
+    clone = "ubuntu-2204-template"
+    cores = 2
+    sockets = 1
+    cpu = "host"
+    memory = 2048
+
+    network{
+        bridge = "vmbr0"
+        model = "virtio"
+    }
+    disk {
+        storage = "local-lvm"
+        type = "virtio"
+        size = "20G"
+    }
+  
+}
